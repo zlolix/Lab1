93e5b7c800d16106b3ef6848ae0861bde7b3567d
zloxi <stermandima@gmail.com>
2026-03-12 21:35:58 +0700

    added task2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <cstring>
+#include <string>
 using namespace std;
 
 char isVowels[] = { 'a', 'e', 'y', 'u', 'i', 'o' };
@@ -12,13 +12,48 @@ size_t CountVowels(const char* stroka) {
 	} return count;
 }
 
-int main() {
-	char input[100];
-	cout << "Введите строку: ";
-	cin.getline(input, 100);
-	size_t result = CountVowels(input);
+enum class Case
+{
+	Upper,
+	Lower
+};
 
-	cout << "Количество гласных: " << result << endl;
+void ConvertCase(std::string& str, Case reg)
+{
+	for (char& c : str)
+	{
+		if (reg == Case::Upper)
+			c = std::toupper(c);
+		else
+			c = std::tolower(c);
+	}
+}
+
+int main() {
+	int choice;
+	std::cout << "choice function: \n 1. CountVowels \n 2. ConvertCase ";
+	std::cin >> choice;
+	cin.ignore();
+	switch(choice) {
+		case 1: 
+			char input[100];
+			cout << "Enter string: ";
+			cin.getline(input, 100);
+			size_t result = CountVowels(input);
+			cout << "Count vowels: " << result << endl;
+			break;
+		case 2:
+			std::string text = "";
+			cout << "Enter string: ";
+			getline(cin, text);
+			std::cout << "Original: " << text << std::endl;
+			ConvertCase(text, Case::Upper);
+			std::cout << "Upper registry: " << text << std::endl;
+			ConvertCase(text, Case::Lower);
+			std::cout << "Lower registry: " << text << std::endl;
+			break;
+	}
+	
 
     return 0;
 }
